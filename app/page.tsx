5982899c7a818eacf52887a21232a1fd57e3f438
FoxedDev <169158948+FoxedDev@users.noreply.github.com>
2025-07-06 13:42:18 +0200

    Replace SVGR with logo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Logo from "@/public/img/logo.svg";
+import Logo from "@/components/Logo";
 import Card from "@/components/Card";
 import toast from "react-hot-toast";
 
---
+++ components/Logo.tsx
@@ -0,0 +1,31 @@
+export default function Logo({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      width="256"
+      height="256"
+      viewBox="0 0 256 256"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M128 224L48 170.667V64L128 117.333L208 64V170.667L128 224Z"
+        stroke="currentColor"
+        stroke-width="12"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M117.333 160L90.6667 144V176L117.333 160Z"
+        stroke="currentColor"
+        stroke-width="12"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M165.333 144L138.667 160L165.333 176V144Z"
+        stroke="currentColor"
+        stroke-width="12"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
