25568575618d6f00bd74d4da5a2dc06390efb89d
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2025-07-13 19:49:18 +0200

    How small can a fix be? (Yes)

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <Section>
-        <p className="font-mono text-gray-500">Made with 💜 by Foxed</p>
+        <p className="font-mono text-gray-500">Made with 💜 by Foxed.</p>
       </Section>
     </footer>
   );
