8595f3dc06aa434942f784fec9657967c9cd5c42
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2025-11-30 15:09:47 +0100

    Fix not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,8 @@
-"use client";
-
 import { FaRegFrown } from "react-icons/fa";
 import headerIconStyles from "@/styles/headerIcon.module.css";
 import titleStyles from "@/styles/title.module.css";
+import SplitText from "@/components/SplitText";
+import Link from "next/link";
 
 export default function Error() {
   return (
@@ -12,9 +12,13 @@ export default function Error() {
           <FaRegFrown className={headerIconStyles.icon} />
           <div className={titleStyles.title}>
             <p className={titleStyles.tagline}>Error</p>
-            <h1>404</h1>
+            <h1>
+              <SplitText text="404" />
+            </h1>
             <p className={titleStyles.subtitle}>
               This page could not be found.
+              <br />
+              <Link href={"/"}>Return to homepage</Link>
             </p>
           </div>
         </section>
